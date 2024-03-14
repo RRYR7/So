1b308c34552fb25877124766cf3e4b261ed85558
MiOZK <163374541+MiOZK@users.noreply.github.com>
2024-03-14 08:47:01 +0300

    Update dir.py

--- AFROTOMusic/core/dir.py
+++ AFROTOMusic/core/dir.py
@@ -17,4 +17,4 @@ def dirr():
     if "cache" not in os.listdir():
         os.mkdir("cache")
 
-    LOGGER("ميــوزك عفرتو").info("تم تحـديث السـورس ...✓")
+    LOGGER("ميــوزك الزعيم").info("تم تحـديث السـورس ...✓")
