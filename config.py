74dd117b3e162e01fc601ca03a73d0c182cdd47a
MiOZK <163374541+MiOZK@users.noreply.github.com>
2024-03-16 00:36:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/abdalaleQ/AFROTOMusic",
+    "https://github.com/MiOZK/AFROTOMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
