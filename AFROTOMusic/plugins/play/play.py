8e812ba477f7b1ce600240a150a20cf28206fe9c
Mohammad Taleb <98346135+RRYR7@users.noreply.github.com>
2024-04-06 17:30:54 +0300

    Update play.py

--- AFROTOMusic/plugins/play/play.py
+++ AFROTOMusic/plugins/play/play.py
@@ -33,7 +33,7 @@ from config import BANNED_USERS, lyrical
             "تشغيل",
             "تش",
             "/play",
-            "/vplay",
+            "فيديو",
             "/cplay",
             "/cvplay",
             "/playforce",
